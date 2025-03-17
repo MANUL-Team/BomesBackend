4fa1e22b73504b1bfa4659da63d62a20a9604a74
Egor Stolbov <prostkadet@gmail.com>
2025-03-17 08:45:41 +0000

    Update 5 files
    - /Services/BomesCryptoService/index.js
    - /Services/BomesCryptoService/.gitlab-ci.yml
    - /Services/BomesCryptoService/.env
    - /Services/BomesCryptoService/package.json
    - /.gitlab-ci.yml

--- Services/BomesCryptoService/index.js
+++ Services/BomesCryptoService/index.js
@@ -1,17 +1,81 @@
-var rsa = require("node-rsa");
-var fs = require("fs");
-// ФУНКЦИЯ ДЛЯ ГЕНЕРАЦИИ КЛЮЧЕЙ: ПУБЛИЧНОГО И ПРИВАТНОГО
-function GeneratePair(){
-    var key = new rsa().generateKeyPair();
+require("dotenv").config();
+const WebSocketClient = require('websocket').client;
+const Utils = require("./Utils");
+const rsa = require("node-rsa");
+const fs = require("fs");
 
-    var publicKey = key.exportKey("public");
-    var privateKey = key.exportKey("private");
+const api_address = process.env.API_ADDRESS;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-    fs.openSync("./Keys/public.pem","w");
-    fs.writeFileSync("./Keys/public.pem",publicKey,"utf-8");
 
-    fs.openSync("./Keys/private.pem","w");
-    fs.writeFileSync("./Keys/private.pem",privateKey,"utf-8");
+let clientsKeys = {};
+
+const client = new WebSocketClient();
+
+client.on("connect", (connection) => {
+    const registerService = {
+        event: "RegisterService",
+        serviceName: "CryptoService",
+        requests: []
+    };
+    connection.sendUTF(JSON.stringify(registerService));
+
+    connection.on("message", async (message) => {
+        message = JSON.parse(message.utf8Data);
+        let request_user = GetRequestUser(message);
+        switch(message.event){
+            case "GeneraeteKeys":
+                GenerateKeysPair(connection, message.clientID);
+                break;
+            case "RemoveKeys":
+                RemoveKeys(message.clientID);
+                break;
+            default:
+                console.log(message);
+        }
+    });
+});
+
+client.connect(api_address, 'echo-protocol');
+
+
+function GetRequestUser(message) {
+    let request_user = message.request_user;
+    if (!request_user) {
+        if (message.request_identifier) {
+            request_user = {
+                identifier: message.request_identifier,
+                password: message.request_password
+            }
+        }
+        else {
+            request_user = {
+                identifier: message.identifier,
+                password: message.password
+            }
+        }
+    }
+    return request_user;
 }
 
-GeneratePair();
+function GenerateKeysPair(ws, clientID){
+    const key = new rsa().generateKeyPair();
+
+    const publicKey = key.exportKey("public");
+    const privateKey = key.exportKey("private");
+
+    clientsKeys[clientID] = {
+        publicKey: publicKey,
+        privateKey: privateKey
+    }
+    const request = {
+        event: "ReturnPublicKey",
+        clientID: clientID,
+        publicKey: publicKey
+    }
+    ws.sendUTF(JSON.stringify(request));
+}
+
+function RemoveKeys(clientID) {
+    clientsKeys[clientID] = undefined;
+}
