2310af461f1c4275f33479d18770a8540b1aba95
Egor Stolbov <prostkadet@gmail.com>
2025-03-18 08:06:32 +0000

    Update file index_dev.js

--- Services/BomesAPI/index_dev.js
+++ Services/BomesAPI/index_dev.js
@@ -70,9 +70,10 @@ wss.on("connection", (ws, req) => {
     RegisterClient(ws);
     ws.on("message", (message) => {
         try{
+            message = message.toString();
             console.log(message);
             if (ws.clientID && connected_clients[ws.clientID] && connected_clients[ws.clientID].key) {
-                message = serverKeys.decrypt(message, ["utf-8"]);
+                message = serverKeys.decrypt(message, "utf-8");
                 console.log(message);
             }
             message = JSON.parse(message);
