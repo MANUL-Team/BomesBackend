aa39c94976e867384b5fdd85820de4f2bf7ac7e2
Egor Stolbov <prostkadet@gmail.com>
2025-05-01 07:16:24 +0000

    Update file index.js

--- Services/BomesWebAuth/index.js
+++ Services/BomesWebAuth/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const request = require('request');
-const Utils = require("/Utils.js");
+const Utils = require("./Utils.js");
 
 const app = express();
 const port = 3000;
