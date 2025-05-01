58c22907564abe182e18dd57f3ecd9201f0e7da2
Egor Stolbov <prostkadet@gmail.com>
2025-05-01 12:24:36 +0000

    Update 2 files
    - /Services/BomesWebAuth/.env
    - /Services/BomesWebAuth/index.js

--- Services/BomesWebAuth/index.js
+++ Services/BomesWebAuth/index.js
@@ -6,10 +6,40 @@ const express = require("express");
 const cors = require("cors");
 const request = require('request');
 const Utils = require("./Utils.js"); // Импорт утилитных функций. См. файл Utils.js
+const mysql = require("mysql2");
 
 const app = express();
+
 const PORT = process.env.PORT;
 const CORE_ADDRESS = process.env.CORE_ADDRESS;
+const database_host = process.env.DATABASE_ADDRESS;
+const database_user = process.env.DATABASE_USERNAME;
+const database_password = process.env.DATABASE_PASSWORD;
+const database_name = process.env.DATABASE_NAME;
+
+const connectionConfig = {
+    host: database_host,
+    user: database_user,
+    password: database_password,
+    database: database_name
+}
+
+let database = mysql.createConnection(connectionConfig);
+database.connect(function (err) {
+    if (err) Utils.error(err);
+    else Utils.log("Success SQL connection!");
+});
+
+const emails = ["Hello", "TestTest", "Abrakadabra"];
+
+for (let i = 0; i < 10000000; i++) {
+    const sql = 'INSERT INTO test_table (email, password) VALUES (?, ?);';
+    const data = [emails[i % emails.length], emails[i % emails.length]];
+    database.query(sql, data, (err, results) => {
+        if (err) Utils.error(err);
+    });
+}
+
 
 app.use(express.json());
 app.use(express.urlencoded());
