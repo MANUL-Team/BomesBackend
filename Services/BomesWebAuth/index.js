2d666bdf940dfac5a6e80dceff4b5ffec3be35e7
Egor Stolbov <prostkadet@gmail.com>
2025-05-01 07:03:49 +0000

    Update 7 files
    - /Services/BomesWebAuth/package.json
    - /Services/BomesWebAuth/package-lock.json
    - /Services/BomesWebAuth/index.js
    - /Services/BomesWebAuth/Dockerfile
    - /Services/BomesWebAuth/.gitlab-ci.yml
    - /.gitlab-ci.yml
    - /compose.yml

---
+++ Services/BomesWebAuth/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const request = require('request');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors({credentials: true, origin: true}));
+
+app.get('/', (req, res) => {
+    console.log("IP: " + req.ip.slice(7));
+    res.send("Successful request!");
+});
+
+app.get("/test_request", (req, res) => {
+    console.log("Test request, IP: " + req.ip.slice(7));
+    res.send("Success from auth");
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
+
+request.post(
+    {
+        url: 'http://172.17.0.3:3000/register_service',
+        form: {
+            data: JSON.stringify({
+                port,
+                requests: [
+                    {
+                        type: "GET",
+                        value: "/test_request"
+                    }
+                ]
+            })
+        }
+    },
+    (err, response, body) => {
+        if (err) console.log(err);
+        else {
+            console.log("REGISTERED!");
+        }
+    }
+);
