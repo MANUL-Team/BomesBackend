9358b8e995e312aa55036aa9156193a2556a7306
Egor Stolbov <prostkadet@gmail.com>
2025-05-01 14:30:38 +0000

    Update file index.js

--- Services/BomesWebAuth/index.js
+++ Services/BomesWebAuth/index.js
@@ -60,6 +60,7 @@ app.post("/get_users", (req, res) => {
     database.query(sql, data, (err, results) => {
         if (err) Utils.error(err);
         else {
+            Utils.log(results);
             res.send(JSON.stringify(results));
         }
     });
