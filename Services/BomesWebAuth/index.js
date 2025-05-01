5ce03b1a632642ad3cb4791dec4608047a5cdd8b
Egor Stolbov <prostkadet@gmail.com>
2025-05-01 07:59:46 +0000

    Update 2 files
    - /Services/BomesWebAuth/index.js
    - /Services/BomesWebCore/index.js

--- Services/BomesWebAuth/index.js
+++ Services/BomesWebAuth/index.js
@@ -1,32 +1,38 @@
+// Импорт библиотек, установка констант и стандартных настроек express
+// #################################
 require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 const request = require('request');
-const Utils = require("./Utils.js");
+const Utils = require("./Utils.js"); // Импорт утилитных функций. См. файл Utils.js
 
 const app = express();
 const PORT = process.env.PORT;
-
 const CORE_ADDRESS = process.env.CORE_ADDRESS;
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors({credentials: true, origin: true}));
+// #################################
 
+// Обработка тестового запроса
 app.get("/test_request", (req, res) => {
-    Utils.log("Test request, IP: " + req.ip.slice(7));
+    Utils.log(`Test request, IP: ${req.ip.slice(7)}`);
     res.send("Success from auth");
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
     Utils.log(`Сервер запущен на порту ${PORT}`);
 });
 
+// Отправка запроса на регистрацию в ядро
 request.post(
     {
         url: `http://${CORE_ADDRESS}/register_service`,
         form: {
+            // Вся информация в виде json-строки в поле data
             data: JSON.stringify({
                 port: PORT,
                 requests: [
