e80501f7e1b2d5245047d5ac9d76682d0002f4d0
Egor Stolbov <prostkadet@gmail.com>
2025-05-02 15:39:50 +0000

    Update 3 files
    - /Services/BomesPythonServiceExample/app.py
    - /Services/BomesPythonServiceExample/Dockerfile
    - /compose.yml

--- Services/BomesPythonServiceExample/app.py
+++ Services/BomesPythonServiceExample/app.py
@@ -1,11 +1,31 @@
 from flask import Flask
+import requests
  
 app = Flask(__name__)
+PORT = 3000
+CORE_ADDRESS = "172.20.1.140:3000"
  
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/python_example")
+def python_example():
+    return "PythonExample"
+
+def register_service():
+    data = {
+        "port": PORT,
+        "requests": [
+            {
+                "type": "GET",
+                "value": "/python_example"
+            }
+        ]
+    }
+    response = requests.post(f"http://{CORE_ADDRESS}/register_service", data=data)
  
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=3001)
+    serve(app, host="0.0.0.0", port=PORT)
+    register_service()
