c65ae61c5d9f3144608cc5cf889bfa41e357c6c1
Egor Stolbov <prostkadet@gmail.com>
2025-12-23 16:44:02 +0000

    Update 3 files
    - /Services/HTTP/AuthService/requirements.txt
    - /Services/HTTP/AuthService/main.py
    - /Services/HTTP/Core/requirements.txt

--- Services/HTTP/AuthService/main.py
+++ Services/HTTP/AuthService/main.py
@@ -1,4 +1,4 @@
-import pika
+import aio_pika
 import json
 import os
 from dotenv import load_dotenv
@@ -6,41 +6,40 @@ from auth_requests import *
 
 load_dotenv()
 
-RABBIT_ADDRESS, RABBIT_PORT = map(str, os.getenv("RABBIT_ADDRESS").split(":"))
+RABBIT_ADDRESS = os.getenv("RABBIT_ADDRESS")
 RABBIT_USERNAME = os.getenv("RABBIT_USERNAME", "guest")
 RABBIT_PASSWORD = os.getenv("RABBIT_PASSWORD", "guest")
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=RABBIT_ADDRESS,
-        port=RABBIT_PORT
-    ))
-    channel = connection.channel()
-
-    channel.queue_declare(queue='auth-1')
-
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body.decode()}")
-        data = json.loads(body.decode())
-        request = data.get("request")
-        core_index = data.get("core_index")
-        response_message = f"Unknown request: {request}"
-        request_data = data.get("data", {})
-        code = 404
-        if request == "/api/auth/register":
-            response_message, code = register(request_data.get("email"), request_data.get("password"))
-        response = {
-            "key": data.get("key"),
-            "message": response_message,
-            "code": code
-        }
-        channel.basic_publish(exchange='',
-                      routing_key=f'core-{core_index}',
-                      body=json.dumps(response, ensure_ascii=False))
-
-    channel.basic_consume(queue='auth-1',
-                          on_message_callback=callback,
-                          auto_ack=True)
+    connection = await aio_pika.connect_robust(
+        f"amqp://{RABBIT_USERNAME}:{RABBIT_PASSWORD}@{RABBIT_ADDRESS}/"
+    )
+    channel = await connection.channel()
+    async def auth_callback(message: aio_pika.IncomingMessage):
+        async with message.process():
+            print(f" [x] Received {message.body.decode()}")
+            data = json.loads(message.body.decode())
+            request = data.get("request")
+            core_index = data.get("core_index")
+            response_message = f"Unknown request: {request}"
+            request_data = data.get("data", {})
+            code = 404
+            if request == "/api/auth/register":
+                response_message, code = register(request_data.get("email"), request_data.get("password"))
+            response = {
+                "key": data.get("key"),
+                "message": response_message,
+                "code": code
+            }
+            await channel.default_exchange.publish(
+                aio_pika.Message(
+                    body=json.dumps(response, ensure_ascii=False),
+                    delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+                ),
+                routing_key=f'core-{core_index}',
+            )
+    auth_queue = await static_data.channel.declare_queue(f'auth-1')
+    await auth_queue.consume(auth_callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
