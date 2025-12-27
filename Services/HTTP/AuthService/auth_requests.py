edf6dc5167483a330f4479018d51ca357d3853be
Egor Stolbov <prostkadet@gmail.com>
2025-12-27 15:11:09 +0000

    Update file auth_requests.py

--- Services/HTTP/AuthService/auth_requests.py
+++ Services/HTTP/AuthService/auth_requests.py
@@ -2,25 +2,19 @@ from datetime import datetime
 
 
 def register(username: str, email: str, password: str):
-    message = {
+    response = {
         "status": "SUCCESS",
         "message": "Code was sent to your email!",
         "timestamp": datetime.now().isoformat(),
         "more": f"Your data: {username}, {email}, {password}"
     }
-    response = {
-        "message": message
-    }
     return response, 200
 
 def login(email: str, password: str):
-    message = {
+    response = {
         "status": "SUCCESS",
         "message": "Successful login",
         "timestamp": datetime.now().isoformat(),
         "more": f"Your data: {email}, {password}"
-    }
-    response = {
-        "message": message
     }
     return response, 200
